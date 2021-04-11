5253ff7c5c2b10f5681f7578748d941519952598
KHAIREDDINE Mohamed <mohamed.khaireddine@gmail.com>
2021-04-11 13:06:39 +0200

    create static factory to create parrots

--- src/main/java/parrot/Parrot.java
+++ src/main/java/parrot/Parrot.java
@@ -7,12 +7,21 @@ public class Parrot {
   private final double voltage;
   private final boolean isNailed;
 
-  public Parrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
+  Parrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
     this.type = type;
     this.numberOfCoconuts = numberOfCoconuts;
     this.voltage = voltage;
     this.isNailed = isNailed;
   }
+
+  public static Parrot createParrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
+    return switch (type) {
+      case EUROPEAN -> new EuropeanParrot(numberOfCoconuts, voltage, isNailed);
+      case AFRICAN -> new AfricanParrot(numberOfCoconuts, voltage, isNailed);
+      case NORWEGIAN_BLUE -> new NorwegianBlueParrot(numberOfCoconuts, voltage, isNailed);
+    };
+  }
+
 
   public double getSpeed() {
     switch (type) {
